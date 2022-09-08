477e0f685801eedc8f48c897f2800e8c333e556d
Korotenko Maksim <korotenko.maksim@mail.ru>
2022-09-08 22:30:16 +1000

    added saving of the filter form in the session data
    closes #1

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.db.models import Q
 from catalog.models import Item, Category
@@ -5,48 +7,54 @@ from .forms import Filter
 
 
 def main(request, categoryId=None):
-    # выполняем обработку формы фильтр
-    querySize = Q()
+    # определим пустые выражения для выборки из модели,
+    # чтобы в самом простом случае выгрузить все
+    queryCategory = Q()
     querySeason = Q()
+
+    data = request.session.get('filterForm', {})
+    filterForm = Filter(initial=data)
+
+    # выполняем обработку формы фильтр
     if request.method == 'POST':
         filterForm = Filter(request.POST)
         if filterForm.is_valid():
             data = filterForm.cleaned_data
-
-            # сначала определим параметры фильтра по размеру, если задан
-            if data['sizeFrom'] is None:
-                data['sizeFrom'] = 0
-            if data['sizeTo'] is None:
-                data['sizeTo'] = 1000
-
-            querySize = Q(size__range=(data['sizeFrom'], data['sizeTo']))
-
-            # теперь определим параметры фильтра по сезону
-            seasonsName = []
-            for season in filterForm.fields:
-                if season not in filterForm.sizeList:
-                    if data[season]:
-                        seasonsName.append(season)
-
-            # если никакой сезон не выбран, значит включаем все
-            if len(seasonsName):
-                querySeason = Q(season__name__in=seasonsName)
+            request.session['filterForm'] = data
         else:
             print('form is not valid')
-    else:
-        filterForm = Filter()
 
-    if categoryId is None:
-        items = Item.objects.filter(querySize & querySeason)
-    else:
+    # сначала определим параметры фильтра по размеру, если задан
+    sizeFrom = data.get('sizeFrom', 0)
+    if sizeFrom is None:
+        sizeFrom = 0
+    sizeTo = data.get('sizeTo', 1000)
+    if sizeTo is None:
+        sizeTo = 1000
+
+    print(str(sizeFrom) + '-' + str(sizeTo))
+
+    querySize = Q(size__range=(sizeFrom, sizeTo))
+
+    # теперь определим параметры фильтра по сезону
+    seasonsName = []
+    for season in filterForm.fields:
+        if season not in filterForm.sizeList:
+            if data.get(season, False):
+                seasonsName.append(season)
+
+    # если никакой сезон не выбран, значит включаем все
+    if len(seasonsName):
+        querySeason = Q(season__name__in=seasonsName)
+
+    if categoryId:
         category = Category.objects.get(id=categoryId)
-
         if category.parentId is None:
             queryCategory = Q(categoryId__parentId=categoryId)
         else:
             queryCategory = Q(categoryId=categoryId)
 
-        items = Item.objects.filter(queryCategory & querySize & querySeason)
+    items = Item.objects.filter(queryCategory & querySize & querySeason)
 
     # вытягиваем все родительские категории
     categories = Category.objects.filter(parentId=None)
